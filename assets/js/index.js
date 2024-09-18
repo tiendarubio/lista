bf6bbc5f5a6c59a4f615053ab7be9456638df57b
tiendarubio <sopti.tiendarubio@gmail.com>
2024-09-18 06:17:49 -0600

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+// index.js
+document.getElementById('sexCalle').addEventListener('click', function () {
+    window.location.href = 'lista.html?store=lista_sexta_calle';
+});
+
+document.getElementById('avenMorazan').addEventListener('click', function () {
+    window.location.href = 'lista.html?store=lista_avenida_morazan';
+});
+
+document.getElementById('centroComercial').addEventListener('click', function () {
+    window.location.href = 'lista.html?store=lista_centro_comercial';
+});
